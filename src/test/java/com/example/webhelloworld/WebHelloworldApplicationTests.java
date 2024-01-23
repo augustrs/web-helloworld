b8a94feac5fed5fac0120d003cdbe9996b8aaebe
augustrs <auso0001@stud.kea.dk>
2024-01-23 16:07:23 +0100

    Initial commit

---
+++ src/test/java/com/example/webhelloworld/WebHelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webhelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebHelloworldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
